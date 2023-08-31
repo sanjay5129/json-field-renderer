08dc352af9d1fcffe5216311d701e86e83b16f56
Sanjay Jain <sanjayjain1529@gmail.com>
2023-08-31 18:47:49 +0530

    feature: added example workflow for button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,16 @@
 import Button from '@mui/material/Button';
 
 export default function ButtonComponent({ text, onClick, workflow }) {
+  const handleClick = e => {
+    if (workflow) {
+      workflow.action(workflow.target);
+    }
+
+    if (onClick) {
+      onClick(e);
+    }
+  };
   return (
-    <Button onClick={onClick}>{text}</Button>
+    <Button onClick={handleClick}>{text}</Button>
   )
 }
--- src/containers/FieldRenderer/useFieldRenderer.jsx
+++ src/containers/FieldRenderer/useFieldRenderer.jsx
@@ -11,6 +11,13 @@ const useFieldRenderer = () => {
       [modalName]: false,
     }));
 
+  const workFlows = () => ({
+    openModal: targetModal => setFieldState(p => ({
+      ...p,
+      [targetModal]: true
+    }))
+  })
+
   const componentMap = componentMapper(handleModalClose);
 
   const fieldRenderer = (data) => {
@@ -23,6 +30,15 @@ const useFieldRenderer = () => {
         name,
         stateValue: fieldState[name],
       };
+      if (data.Content.props.secondaryAction) {
+        propValue = {
+          ...propValue,
+          workflow: {
+            action: workFlows()[data.Content.props.secondaryAction.type],
+            target: data.Content.props.secondaryAction.target
+          }
+        }
+      }
 
       if (children) {
         return (
