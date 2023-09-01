092f0085bc50eec3e42b33c648043cb9d8430a34
Sanjay Jain <sanjayjain1529@gmail.com>
2023-09-01 13:42:22 +0530

    feature: added optional bullet

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,11 +2,18 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 
-export default function ListComponent({ li }) {
+export default function ListComponent({ li, bulletList }) {
+  let style = {};
+  if (bulletList) {
+    style = {
+      list: { listStyleType: 'disc', pl: 4 },
+      listItem: { display: 'list-item' },
+    }
+  }
   return (
-    <List>
+    <List sx={style.list}>
       {li.map(item => (
-        <ListItem key={li.id || li}>
+        <ListItem sx={style.listItem} key={li.id || li}>
           <ListItemText primary={item} />
         </ListItem>
       ))}
