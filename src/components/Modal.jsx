1ec2b6d59dc648f6849604d01f8d8f61eb4c685c
Sanjay Jain <sanjayjain1529@gmail.com>
2023-09-01 14:06:15 +0530

    chore: added modal background

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -8,6 +8,10 @@ export default function ModalComponent({ isOpen, name, children, stateValue, han
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
+        bgcolor: 'white',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
         ...rest,
       }}
       onClose={handleModalClose(name)}
